ba2e9513787e044601f89664053bb0fe0fcefbac
Bharath <bharath.s@kas-services.com>
2024-10-05 10:51:41 +0530

    [main] adding simple page.

---
+++ app/pages/main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def generate_page():
+    """_summary_"""
+
+    st.set_page_config(page_title="Ground Water Monitor", page_icon=":books:")
+    st.title("Ground Water Monitor")
